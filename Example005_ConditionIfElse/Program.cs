3b6fa3c3e7453b765c1fba8c29656e2d64d363e7
Michael <minecraft4455@yandex.ru>
2023-01-28 13:10:52 +0300

    add Exaple 005 ConditionIfElse(ToLower)

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША");
 }
