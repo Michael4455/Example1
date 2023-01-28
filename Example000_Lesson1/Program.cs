3ea88b866f7ccf25c9c28665bccf89bb35bbe7ed
Michael <minecraft4455@yandex.ru>
2023-01-28 17:09:33 +0300

    add Example 000 Lesson 1

---
+++ Example000_Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число:");
+
+string numberStr = Console.ReadLine() ??"";
+int number = int.Parse(numberStr);
+int result = number * number;
+
+Console.WriteLine($"квадрта числа {number} = {result}");
